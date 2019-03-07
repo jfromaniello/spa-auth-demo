bfcf38df4393c382ddacba1e8ef5b0377919354d
Steve Hobbs <elkdanger@gmail.com>
2019-03-07 15:54:30 +0000

    Got the basic app up and running

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,124 @@
+var clientStateMixin = {
+  data() {
+    const { user, isAuthenticated } = window.clientState;
+
+    return {
+      user,
+      isAuthenticated
+    };
+  }
+};
+
+Vue.component('Navigation', {
+  mixins: [clientStateMixin],
+  template: `
+    <div>
+      <router-link to="/" class="nav-link">Home</router-link>
+      <router-link to="/profile" class="nav-link">Profile</router-link>
+      <span v-if="isAuthenticated" style="float: right">Logged in as <router-link to="/profile"><strong>{{ user.name }}</strong></router-link> | <a href="/logout">Log out</a></span>
+    </div>
+  `
+});
+
 Vue.component('App', {
   template: `
     <v-app>
       <v-content>
+        <div id="nav">
+          <v-container>
+            <navigation />
+          </v-container>
+        </div>
+
         <v-container>
-          <Home />
+          <router-view />
         </v-container>
       </v-content>
     </v-app>
   `
 });
 
-Vue.component('Home', {
+var Home = Vue.component('Home', {
+  mixins: [clientStateMixin],
   template: `
     <div>
       <h1>SPA OIDC Demo Application</h1>
-      <p>This is an application designed to demonstrate how to authenticate a SPA when
-        you also have a backend server.</p>
+      <p>This is an application designed to demonstrate how to authenticate a SPA when you also have a backend server.</p>
 
-      <v-btn color="info" href="/login">Log in</v-btn>
+      <div v-if="!isAuthenticated">
+        <v-btn color="info" href="/login">Log in</v-btn>
+      </div>
+
+      <div v-if="isAuthenticated">
+        <v-btn color="info" @click="callApi">Call API</v-btn>
+
+        <div id="server-response" v-if="hasResponse">
+          <p>Response from the API call:</p>
+          <pre>{{ JSON.stringify(serverResponse, {}, 2).trim() }}</pre>
+        </div>
+
+      </div>
+    </div>
+  `,
+  data() {
+    return {
+      hasResponse: false,
+      serverResponse: {}
+    };
+  },
+  methods: {
+    callApi() {
+      axios.get('/api').then(({ data }) => {
+        this.serverResponse = data;
+        this.hasResponse = true;
+      });
+    }
+  }
+});
+
+var Profile = Vue.component('Profile', {
+  mixins: [clientStateMixin],
+  template: `
+    <div id="profile">
+      <v-layout row wrap justify-start>
+        <v-layout column shrink>
+          <v-flex>
+              <v-avatar>
+              <img :src="user.picture" />
+            </v-avatar>
+          </v-flex>
+        </v-layout>
+
+        <v-layout column style="margin-left: 20px">
+          <v-flex grow>
+            <h1>{{user.name}}</h1>
+          </v-flex>
+          <v-flex>
+            {{user.email}}
+          </v-flex>
+          <v-flex><strong>Nickname</strong>: {{user.nickname}}</v-flex>
+        </v-layout>
     </div>
   `
 });
 
+var routes = [
+  {
+    path: '/',
+    component: Home
+  },
+  {
+    path: '/profile',
+    component: Profile
+  }
+];
+
+var router = new VueRouter({ routes, mode: 'history' });
+
 var app = new Vue({
   el: '#app',
-  template: '<App/>'
+  template: '<App/>',
+  router
 });
+
+console.log(window.clientState);
--- server/index.js
+++ server/index.js
@@ -5,16 +5,58 @@ const debug = require('debug')('app:server');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { auth } = require('express-openid-connect');
 const { join } = require('path');
+const hbs = require('express-hbs');
 
 const app = express();
 const appSecret = process.env.APP_SECRET || 'keyboard cat';
 
+// Middleware
 app.use(helmet());
 app.use(morgan('dev', { stream: { write: m => debug(m) } }));
-app.use(session({ secret: appSecret }));
+
+app.use(
+  session({ secret: appSecret, resave: false, saveUninitialized: false })
+);
+
 app.use(cookieParser(appSecret));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(join(__dirname, '../', 'public')));
+
+hbs.registerHelper('json', obj => {
+  return new hbs.SafeString(JSON.stringify(obj));
+});
+
+const userInfo = openIdUser => {
+  const { email, name, nickname, picture } = openIdUser || {};
+  return { email, name, nickname, picture };
+};
+
+// View engine
+app.engine(
+  'hbs',
+  hbs.express4({
+    defaultLayout: join(__dirname, '..', 'views', 'layout.hbs')
+  })
+);
+
+app.set('view engine', 'hbs');
+app.set('views', join(__dirname, '..', 'views'));
+
+// Api routing
+app.use('/api', auth(), require('./api'));
+
+app.get('/*', auth({ required: false }), (req, res) => {
+  res.render('index', {
+    clientState: {
+      user: userInfo(req.openid.user),
+      isAuthenticated: req.openid.user !== undefined
+    }
+  });
+});
+
+app.use(auth());
 
 module.exports = app;
